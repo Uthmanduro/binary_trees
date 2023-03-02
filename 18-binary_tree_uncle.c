99aedac32a7ce3394a1e41d8a1d0343335f3c6c0
uthmanduro <udurosinlohun@gmail.com>
2023-03-02 08:10:24 -0800

    mandatory done

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -9,7 +9,8 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 	binary_tree_t *elder;
 
 	elder = node->parent;
-	if (!node || !elder || !elder->parent)
+	if (!node || !elder || !elder->parent || (!elder->parent->left ||
+						  !elder->parent->right))
 		return (NULL);
 	if (elder == elder->parent->left)
 		return (elder->parent->right);
